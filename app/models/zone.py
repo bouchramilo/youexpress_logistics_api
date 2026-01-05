ee6284b88081da6fd97a963421542a278534658e
epsilon403 <epsilon403@github.com>
2026-01-05 15:59:39 +0100

    adding the zone model

---
+++ app/models/zone.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+class Zone(Base):
+    __tablename__ = "zones"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    nom = Column(String, nullable=False, index=True)
+    ville = Column(String, nullable=False, index=True)
+    code_postal = Column(String, nullable=True)
+    colis = relationship("Colis", back_populates="zone")
