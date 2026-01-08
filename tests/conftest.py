7076a2d38e1e6a794a612cbf78b5461bf7f81580
bouchramilou <miloudybouchra01@gmail.com>
2026-01-08 09:07:01 +0100

    YLA-38 Service, Router, configuration for testing

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.main import app
+from app.core.database import Base, get_db
+from app.core.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = settings.database_url
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="function")
+def test_db():
+    
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture(scope="function")
+def client(test_db):
+    def override_get_db():
+        try:
+            yield test_db
+        finally:
+            test_db.close()
+    
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as c:
+        yield c
+        
